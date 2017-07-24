3686dffe41b945abc4e2b8797abc6f5d53f65faa
Andrew Garyga <andrewgaryga@gmail.com>
2017-07-24 10:31:20 +0300

    beta 2

--- js/custom.js
+++ js/custom.js
@@ -25,6 +25,22 @@ jQuery(document).ready(function($) {
         if    (   el.hasClass("already-visible") == false  ) {
           if ( $(".books-content:last-child").hasClass("come-in")  ) { mixCheck = true; }
             el.addClass("come-in"); 
+
+            // Header
+
+            // SUBHEADER
+            el.children().children(".sub-header").addClass("sub-header-animation");
+            //$(".sub-header").addClass("sub-header-animation");
+
+
+            // Header Books
+            el.children().children(".header-books").addClass("sub-header-animation");
+            // $(".header-books").addClass("header-books-animation");
+
+
+            // BIG img
+            el.children().next().children().addClass("big-image-animation");
+            // $(".big-image").addClass("big-image-animation");
           } 
         // second if end
         } 
